1ddc1623327b85ed420b91558ae0951d7ea420f4
Nishith Savla <nishithsavla005@gmail.com>
2023-10-31 16:23:51 +0530

    Restructure files according to MVC architecture

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,31 +1,12 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
+
+func blah() {
+	panic("...")
+}
 
 func main() {
-	err := CreateOrg()
-	fmt.Println(err)
-}
-func Connect() error {
-	return errors.New("Connection Failed")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user :%w", err)
-	}
-	//..continue...
-	return nil
-}
-
-func CreateOrg() error {
-	err := CreateUser()
-	if err != nil {
-		return fmt.Errorf("Create Org : %w", err)
-	}
-	return nil
+	numbers := []int{1, 2, 3.}
+	fmt.Println(numbers[4])
 }
