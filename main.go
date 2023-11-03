f0434fa4a461352b81379091cf770bf0756afc7d
Nishith Savla <nishithsavla005@gmail.com>
2023-11-03 18:33:20 +0530

    Parse templates from FS to embed in binary

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/Nishith-Savla/lenslocked/controllers"
+	"github.com/Nishith-Savla/lenslocked/templates"
 	"github.com/Nishith-Savla/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -13,13 +13,14 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
 	fmt.Println("starting server on :3000...")
