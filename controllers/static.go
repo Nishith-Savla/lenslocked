b6bcfdd12b795f6d727fb2b9714b15cc749cfab2
Nishith Savla <nishithsavla005@gmail.com>
2024-01-02 23:53:07 +0530

    Made FAQs Dynamic

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/Nishith-Savla/lenslocked/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	question := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free Version ?",
+			Answer:   "Yes We do offer a 30 Day Free trial",
+		},
+		{
+			Question: "What Are you support hours ?",
+			Answer:   "24/7",
+		},
+		{
+			Question: "Contact Options ?",
+			Answer:   ` Email us - <a href="mailto:nishithsavla005@gmail.com">nishithsavla005@gmail.com</a>`,
+		},
+		{
+			Question: "Office Location?",
+			Answer:   "Our entire team is remote",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, question)
 	}
 }
