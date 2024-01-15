36d75efd9f5ca6a8416a75e77a5be5b89ccfbbdd
Nishith Savla <nishithsavla005@gmail.com>
2024-01-15 16:13:15 +0530

    Query multiple rows

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -30,6 +29,7 @@ func main() {
 		Database: "lenslocked",
 		SSLMode:  "disable",
 	}
+	fmt.Printf("%s", cfg)
 	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
@@ -81,14 +81,64 @@ func main() {
 	// }
 	// fmt.Println("User created!, id:", id)
 
-	id := 1
-	row := db.QueryRow(`
-		SELECT name, email FROM users WHERE id = $1;
-	`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
-	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+	// id := 4
+	// row := db.QueryRow(`
+	// 	SELECT name, email FROM users WHERE id = $1;
+	// `, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err != nil && !errors.Is(err, sql.ErrNoRows) {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User information: name=%q email=%q\n", name, email)
+
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err = db.Exec(`
+	// 		INSERT INTO orders (user_id, amount, description)
+	// 		VALUES ($1, $2, $3);
+	// 	`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Created fake orders!")
+
+	type Order struct {
+		ID          int16
+		UserID      int16
+		Amount      int
+		Description string
+	}
+
+	var (
+		orders []Order
+		userID int16 = 1
+	)
+	rows, err := db.Query(`
+		SELECT id, amount, description FROM orders WHERE user_id = $1;
+	`, userID)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("User information: name=%q email=%q\n", name, email)
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(rows)
+	for rows.Next() {
+		order := Order{UserID: userID}
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	if err := rows.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Orders: %#v\n", orders)
 }
