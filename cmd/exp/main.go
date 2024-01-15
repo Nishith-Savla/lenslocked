0b348266bfe8b37aaf664ec1b991c025c0c009c6
Nishith Savla <nishithsavla005@gmail.com>
2024-01-15 15:54:55 +0530

    Get record from db

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -66,17 +67,28 @@ func main() {
 	}
 	fmt.Println("Tables created!")
 
-	name := "Nishith"
-	email := "nish@email.com"
 	// Insert some data
+	// name := "Nishith"
+	// email := "nish@email.com"
+	// row := db.QueryRow(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2) RETURNING id;
+	// `, name, email)
+	// var id int16
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User created!, id:", id)
+
+	id := 1
 	row := db.QueryRow(`
-		INSERT INTO users (name, email)
-		VALUES ($1, $2) RETURNING id;
-	`, name, email)
-	var id int16
-	err = row.Scan(&id)
-	if err != nil {
+		SELECT name, email FROM users WHERE id = $1;
+	`, id)
+	var name, email string
+	err = row.Scan(&name, &email)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		panic(err)
 	}
-	fmt.Println("User created!, id:", id)
+	fmt.Printf("User information: name=%q email=%q\n", name, email)
 }
